45bdc9aee2468d9a34827bf5f2c94874f905a581
porperza <pikza_classic@hotmail.com>
2024-10-04 17:58:30 +0700

    cors install

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import customerInfoRoutes from './routes/customerInfoRoutes.js'; // นำเข้าเส้นทาง
+import cors from 'cors';
 
 const app = express();
 const port = 5000;
 
+app.use(cors());
 app.use(express.json());
 
 // กำหนดเส้นทาง
