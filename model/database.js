54040333a0e2a2764853a5ffb6236c5a7ba6a352
porperza <pikza_classic@hotmail.com>
2024-10-04 22:21:01 +0700

    model/database.js

--- model/database.js
+++ model/database.js
@@ -4,20 +4,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
-// const connection = mysql.createConnection(process.env.DATABASE_URL);
-
 connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL database:', err);
-    } else {
-        console.log('Connected to MySQL database');
-    }
+  if (err) {
+    console.error('Error connecting to MySQL database:', err);
+  } else {
+    console.log('Connected to MySQL database');
+  }
 });
 
 export default connection;
