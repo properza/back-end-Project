89f9c8d5fd05c5ccd5ac4297a2ec0763613fc1ba
porperza <pikza_classic@hotmail.com>
2024-10-04 23:05:23 +0700

    package.json

--- model/database.js
+++ model/database.js
@@ -3,19 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// const connection = mysql.createConnection({
-//     host: process.env.DB_HOST || 'localhost',
-//     user: process.env.DB_USER || 'root',
-//     password: process.env.DB_PASSWORD || '',
-//     database: process.env.DB_DATABASE || 'project_db'
-// });
-
 const connection = mysql.createConnection({
-    host: 'db',
-    user: 'backendProject',
-    password: 'P@ssword312',
-    database: 'project_db'
-  });
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
 
 connection.connect((err) => {
   if (err) {
