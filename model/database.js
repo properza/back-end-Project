8f723fbb996867f7639a7691f5c5c488559c63ca
porperza <pikza_classic@hotmail.com>
2024-10-04 22:39:23 +0700

    model/database.js

--- model/database.js
+++ model/database.js
@@ -3,6 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// ตรวจสอบว่าค่าตัวแปรสิ่งแวดล้อมถูกส่งมาถูกต้อง
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_DATABASE:', process.env.DB_DATABASE);
+
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
