cf65898786b345b05b35b1158bf72b85244ee4d0
porperza <pikza_classic@hotmail.com>
2025-01-03 09:41:36 +0700

    middleware/fileUpload.js

--- middleware/fileUpload.js
+++ middleware/fileUpload.js
@@ -1,7 +1,6 @@
 import multer from 'multer';
 import path from 'path';
 
-// ตั้งค่าการจัดเก็บไฟล์
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'uploads/'); // โฟลเดอร์สำหรับเก็บไฟล์
@@ -13,6 +12,14 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage });
-
-export default upload;
+export const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // จำกัดขนาดไฟล์ (5MB)
+  fileFilter: (req, file, cb) => {
+    const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+    if (!allowedTypes.includes(file.mimetype)) {
+      return cb(new Error('Only .jpg, .jpeg, and .png formats are allowed!'));
+    }
+    cb(null, true);
+  },
+});
