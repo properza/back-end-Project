6326a25a705bdd490b384cc99c801ecd0715eea4
porperza <pikza_classic@hotmail.com>
2025-01-03 09:34:29 +0700

    app.js

--- app.js
+++ app.js
@@ -4,15 +4,23 @@ import adminRoutes from './routes/admin.route.js'
 import eventRoutes from './routes/event.route.js';
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from 'url'; // ใช้เพื่อสร้าง __dirname
+
+// สร้าง __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/admin',adminRoutes)
-app.use('/customer',customerInfoRoutes)
-app.use('/events',eventRoutes)
+// เส้นทางต่างๆ
+app.use('/admin', adminRoutes);
+app.use('/customer', customerInfoRoutes);
+app.use('/events', eventRoutes);
+
+// เส้นทาง Static
 app.use('/utils/gfiles', express.static(path.join(__dirname, 'utils/gfiles')));
 
 app.listen(process.env.port || 4000);
