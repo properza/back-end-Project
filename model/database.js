fbf2f4f7b386dae12aaba1d648442249a72520de
porperza <pikza_classic@hotmail.com>
2025-01-13 19:33:56 +0700

    model/database.js

--- model/database.js
+++ model/database.js
@@ -4,27 +4,29 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-let connection;
+//pool connection
+const connection = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10, // ปรับตามความต้องการ
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: true
+  }
+});
 
-// ฟังก์ชันสำหรับการเชื่อมต่อฐานข้อมูล
-async function connectToDatabase() {
-    try {
-        connection = await mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            // เพิ่มการตั้งค่าอื่นๆ ตามต้องการ
-        });
-        console.log('Connected to MySQL database');
-    } catch (err) {
-        console.error('Error connecting to MySQL database:', err);
-        // ลองเชื่อมต่อใหม่หลังจากเวลาหน่วง (เช่น 2 วินาที)
-        setTimeout(connectToDatabase, 2000);
-    }
-}
-
-// เรียกใช้ฟังก์ชันเชื่อมต่อเมื่อเริ่มต้น
-connectToDatabase();
+// ทดสอบการเชื่อมต่อ
+connection.getConnection()
+  .then(conn => {
+    console.log('Connected to MySQL database');
+    conn.release();
+  })
+  .catch(err => {
+    console.error('Error connecting to MySQL database:', err);
+  });
 
 export default connection;
