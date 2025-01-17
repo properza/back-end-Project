dd55a89908eb5c81362695c21d9203d33a671a94
porperza <pikza_classic@hotmail.com>
2025-01-17 21:36:35 +0700

    model/database.js

--- model/database.js
+++ model/database.js
@@ -1,16 +1,20 @@
+// database.js
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connection = await mysql.createConnection(process.env.DATABASE_URL);
+let connection;
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL database:', err);
-  } else {
+async function connectDB() {
+  try {
+    connection = await mysql.createConnection(process.env.DATABASE_URL);
     console.log('Connected to MySQL database');
+  } catch (err) {
+    console.error('Error connecting to MySQL database:', err);
   }
-});
+}
+
+await connectDB();
 
 export default connection;
